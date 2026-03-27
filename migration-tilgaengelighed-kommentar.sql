7329b740ccea758d8ad0285e25b229dc69dc8c2d
knud6969 <knud.aundal@gmail.com>
2026-03-27 14:11:34 +0100

    db og mock data

--- migration-tilgaengelighed-kommentar.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Tilføj kommentar til tilgaengelighed
--- Kør én gang: psql -U postgres -d vikarsystem -f migration-tilgaengelighed-kommentar.sql
-
-ALTER TABLE tilgaengelighed
-  ADD COLUMN IF NOT EXISTS kommentar VARCHAR(255);
